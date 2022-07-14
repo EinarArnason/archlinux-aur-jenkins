4587e815b33cfacd5aec0b2e93735d223c4cbc91
Einar Arnason <einsiarna@gmail.com>
2022-07-14 22:35:26 +0000

    Create aur.groovy

---
+++ aur.groovy
@@ -0,0 +1,38 @@
+
+def call(String pkgName, String destination, String repoName, String buildCommand) {
+    pipeline {
+        agent any
+
+        stages {
+            stage('build') {
+                steps {
+                    git "https://aur.archlinux.org/${pkgName}.git"
+                    script {
+                        def pkg = sh(returnStdout: true, script: "PKGDEST=${destination} makepkg --packagelist").trim()
+                        def exitCode = sh(returnStatus: true, script: "if [ -f ${pkg} ]; then exit 13; else PKGDEST=${destination} ${buildCommand}; fi")
+                        if (exitCode == 13) {
+                            currentBuild.result = 'ABORTED'
+                            error('Already built')
+                        } else if (exitCode != 0) {
+                            currentBuild.result = 'FAILURE'
+                            error('Failed')
+                        }
+                    }
+                }
+            }
+            
+            stage('Deploy') {
+                steps {
+                    script {
+                        def pkg = sh(returnStdout: true, script: "PKGDEST=${destination} makepkg --packagelist").trim()
+                        def exitCode = sh(returnStatus: true, script: "repo-add ${destination}/${repoName}.db.tar.gz ${pkg}")
+                        if (exitCode != 0) {
+                            currentBuild.result = 'FAILURE'
+                            error('Failed')
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
